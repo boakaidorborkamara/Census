633f15614b1a5f7073b7524a150b1c4620eefdd9
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-06-01 23:18:20 +0000

    first commit after started working with json file

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// all the data from the census
+var census_data = data;
+console.log(census_data);
+
+//targeting html element for data display
+let total_population = document.getElementById("total-population");
+console.log(total_population);
+
+let total_female = document.getElementById("total-female");
+console.log(total_female);
+
+let total_male = document.getElementById("total-male");
+console.log(total_male);
+
+
+//calculate total population
