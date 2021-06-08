de5376de00aa8e145683241cdce79361bedec693
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-06-08 22:38:35 +0000

    Commit done after displaying actual data in first section

--- script/script.js
+++ script/script.js
@@ -1,16 +1,64 @@
 // all the data from the census
 var census_data = data;
-console.log(census_data);
+console.log(census_data.population);
+
 
 //targeting html element for data display
 let total_population = document.getElementById("total-population");
-console.log(total_population);
-
 let total_female = document.getElementById("total-female");
-console.log(total_female);
-
 let total_male = document.getElementById("total-male");
-console.log(total_male);
+
+//Display all the data on the page
+function displayData(){
+    let female = getTotalFemale();
+    let male = getTotalMale();
+    total_female.innerHTML = male;
+    total_male.innerHTML = female;
+    total_population.innerHTML = female + male;
+}
+
+//calculate the total amount of female in the entire country
+function getTotalFemale(){
+    let total_amount_of_female = 0;
+    
+    census_data.population.forEach(ele => {
+        total_amount_of_female += ele.female;
+    });
+    
+    return total_amount_of_female;
+}
+
+//calculate the total amount of male in the entire country
+function getTotalMale(){
+    let total_amount_of_male = 0;
+    
+    census_data.population.forEach(ele => {
+        total_amount_of_male += ele.male;
+    });
+    
+    return total_amount_of_male;
+}
+
+//calculate the total population
+function getTotalPopulation(amount_of_male, amount_of_female){
+    let country_population = amount_of_female + amount_of_female;
+    return country_population;
+}
+
+//get all the counties in the country
+function getCounties(){
+    let counties = [];
+    census_data.population.forEach((ele)=>{
+        counties.push(ele.county);
+    })
+    console.log(counties);
+}
+
+getCounties();
+
+displayData();
+
+
 
 
 //implemention of population for all counties in the county using bar chart
@@ -25,20 +73,7 @@ let all_counties_bar_chart = new Chart(all_counties_ctx, {
             label: '# of Votes',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                 "#519872"
             ],
             borderWidth: 1
         }]
@@ -59,28 +94,16 @@ let female_and_male_ctx = document.getElementById("dognut-chart");
 let female_and_male_chart = new Chart(female_and_male_ctx, {
     type: "pie",
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19,],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+                "white",
+                "#519872"
+            ]
+        }],
+        labels: ["Male", "Female"],
+    }
     
 })
+
